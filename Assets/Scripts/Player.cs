497221ad1d93fa363a49ebc4097057f9bb89af0d
Rodwel Polanco Martinez <rodwelpolancomartinez@gmail.com>
2021-01-10 22:51:11 -0400

    Add animation jump, attack, idle, falling, running

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,29 +4,70 @@ public class Player : MonoBehaviour
 {
     // Config Variables
     [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
 
     // Cache components references
-    Rigidbody2D myridigBody2D;
     Animator myAnimator;
+    Rigidbody2D myridigBody2D;
+
+    BoxCollider2D _myFeetCollider;
+    CapsuleCollider2D _bodyCollider;
 
     private void Start()
     {
-        myridigBody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myridigBody2D = GetComponent<Rigidbody2D>();
+        _myFeetCollider = GetComponent<BoxCollider2D>();
+        _bodyCollider = GetComponent<CapsuleCollider2D>();
     }
     void Update()
     {
         Run();
+        Jump();
+        Falling();
         FlipSprite();
+        Attack();
     }
 
     private void Run()
     {
-        float flowControl = Input.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(flowControl * playerSpeed, myridigBody2D.velocity.y);
+        float flowControl = Input.GetAxis("Horizontal") * playerSpeed;
+        Vector2 playerVelocity = new Vector2(flowControl, myridigBody2D.velocity.y);
         myridigBody2D.velocity = playerVelocity;
         bool playerHorizontalSpeed = Mathf.Abs(myridigBody2D.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("IsRunning", playerHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        var ground = LayerMask.GetMask("Ground");
+        if (!_myFeetCollider.IsTouchingLayers(ground)) { return; }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myridigBody2D.velocity += jumpVelocityToAdd;
+            myAnimator.SetTrigger("IsJumping");
+        }
+    }
+
+    private void Falling()
+    {
+        if (myridigBody2D.velocity.y < -0.1)
+        {
+            myAnimator.SetBool("IsFalling", true);
+        }
+        else
+        {
+            myAnimator.SetBool("IsFalling", false);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            myAnimator.SetTrigger("IsAttacking");
+        }
     }
 
     private void FlipSprite()
@@ -35,8 +76,7 @@ public class Player : MonoBehaviour
         if (playerHorizontalSpeed)
         {
             float rotation = Mathf.Sign(myridigBody2D.velocity.x);
-            if (rotation == 1) transform.eulerAngles = new Vector3(0, 0, 0);
-            if (rotation == -1) transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.localScale = new Vector3(rotation, 1f, 1f);
         }
     }
 }
